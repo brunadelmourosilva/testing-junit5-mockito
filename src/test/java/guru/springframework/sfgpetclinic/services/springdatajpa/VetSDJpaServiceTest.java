1275c6b88f0ba3ebc49b872d78e17b614d222a68
Bruna <brunadelmouro@gmail.com>
2022-06-17 16:24:26 -0300

    Assignment - Write Mockito Tests for Vet Service

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VetSDJpaServiceTest.java
@@ -0,0 +1,36 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.repositories.VetRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+
+@ExtendWith(MockitoExtension.class)
+class VetSDJpaServiceTest {
+
+    @Mock
+    VetRepository vetRepository;
+
+    @InjectMocks
+    VetSDJpaService vetSDJpaService;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void deleteById() {
+        vetSDJpaService.deleteById(1L);
+
+        then(vetRepository).should(times(1)).deleteById(1L);
+    }
+}
